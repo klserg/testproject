5d7ca236a6859fdc9e32cbaab1f6514d04fc79ba
Serg Klopenko <sergey.klopenko@gmail.com>
2022-03-20 17:37:14 +0200

    new state

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("New commit");
-        System.out.println("New");
-        System.out.println("t");
     }
 }
