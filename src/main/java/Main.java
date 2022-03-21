8ca4a81a006f7d6e5236775d3e500b6960cba5ee
Serg Klopenko <sergey.klopenko@gmail.com>
2022-03-21 21:17:45 +0200

    master

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("user1 feature1");
+        System.out.println("master");
         System.out.println("user1 feature1.2");
         System.out.println("user1 feature2");
         System.out.println("user1 feature2.1");
