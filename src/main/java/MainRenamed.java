d26ca1733d00271569ff792dffa30cb9cf66271b
Serg Klopenko <sergey.klopenko@gmail.com>
2022-05-24 14:46:40 +0300

    renamed

--- src/main/java/MainRenamed.java
+++ src/main/java/MainRenamed.java
@@ -1,8 +1,9 @@
-public class Main2 {
+public class MainRenamed {
     public static void main(String[] args) {
         System.out.println("user2 feature 3");
         System.out.println("finished");
         System.out.println("plus ammend");
         System.out.println("user1 feature7");
+        System.out.println("user2 renamed");
     }
 }
