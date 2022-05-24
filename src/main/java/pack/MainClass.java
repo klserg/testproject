86c73a002226f3fc421313a396aee731ba1e1673
Serg Klopenko <sergey.klopenko@gmail.com>
2022-05-24 16:35:32 +0300

    new2

--- src/main/java/pack/MainClass.java
+++ src/main/java/pack/MainClass.java
@@ -4,5 +4,6 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("changed");
+        System.out.println("changed2");
     }
 }
