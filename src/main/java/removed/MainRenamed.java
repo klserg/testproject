b5c00a6a92b274f51b7cefde3a3dc8a469a8acc7
Serg Klopenko <sergey.klopenko@gmail.com>
2022-05-24 16:07:41 +0300

    wihout

--- src/main/java/removed/MainRenamed.java
+++ src/main/java/removed/MainRenamed.java
@@ -1,3 +1,5 @@
+package removed;
+
 public class MainRenamed {
     public static void main(String[] args) {
         System.out.println("user2 feature 3");
@@ -5,5 +7,6 @@ public class MainRenamed {
         System.out.println("plus ammend");
         System.out.println("user1 feature7");
         System.out.println("user2 renamed");
+        System.out.println("user1 removed");
     }
 }
